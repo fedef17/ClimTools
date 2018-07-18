b359277bad4ad8308d79e6115e8c257cc52837aa
Federico Fabiano <f.fabiano@isac.cnr.it>
2018-07-18 18:23:08 +0200

    OK smonta ensemble

--- smonta_ensemble.py
+++ smonta_ensemble.py
@@ -6,28 +6,32 @@ import sys
 import os
 from matplotlib import pyplot as plt
 import netCDF4 as nc
-import cartopy.crs as ccrs
+#import cartopy.crs as ccrs
 from numpy import linalg as LA
 import pickle
-from eofs.standard import Eof
+#from eofs.standard import Eof
 
-cart = '/home/fabiano/Research/lavori/MedscopeEnsClus/DATA/Medscope_seasonal_forecasts/'
+#cart = '/home/fabiano/Research/lavori/MedscopeEnsClus/DATA/Medscope_seasonal_forecasts/'
+cart = '/home/federico/work/MARS_data/MEDSCOPE_seasonal/'
 
 # Open nc file
 years = range(1993,2018)
 nmon = 7
 
-par = 167
-# par = 228
+# par = 167
+# parname = '2t'
+par = 228
+parname = 'tprate'
 
 all_fields = dict()
 
 for year in years:
     for seas in ['may','nov']:
-        nomefile = cart+'seasonal_{}_{}_{}.nc'.format(year, seas, par)
+        nomefile = 'seasonal_{}_{}_{}.nc'.format(year, seas, par)
+        print(nomefile)
 
-        data = nc.Dataset(cart+filename)
-        field = data.variables['2t']
+        data = nc.Dataset(cart+nomefile)
+        field = data.variables[parname]
 
         gigi = field[:,:,:]
         month_preds = np.split(gigi, nmon, axis = 0)
@@ -52,6 +56,7 @@ for seas in ['may','nov']:
         matric = all_fields[(seas, years[0], mon)]
 
         for year in years_clim[1:]:
+            print(seas, year, mon)
             matric = np.concatenate([matric, all_fields[(seas, year, mon)]], axis = 0)
 
         print(matric.shape)
@@ -61,4 +66,8 @@ for seas in ['may','nov']:
     climat_mean[seas] = np.array(climat_mean[seas])
     climat_std[seas] = np.array(climat_std[seas])
 
-pickle.dump([all_fields, climat_mean, climat_std], file=open(cart+'all_fields_climat.p','w'))
+pickle.dump([all_fields, climat_mean, climat_std], open(cart+'all_fields_climat_{}.p'.format(par),'wb'), protocol = 2)
+
+print(climat_mean['may'].shape)
+print(np.mean(climat_mean['may']))
+
