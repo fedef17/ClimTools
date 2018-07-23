f946f8568db88363451c79a8ea0fb00d11529124
Federico Fabiano <f.fabiano@isac.cnr.it>
2018-07-23 20:23:39 +0200

    Improved visual traj

--- test_trajectory_visualization.py
+++ test_trajectory_visualization.py
@@ -24,8 +24,13 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
 from matplotlib.animation import ImageMagickWriter as gif_maker
 
-sys.path.insert(0,'/home/fedefab/Scrivania/Research/Post-doc/git/WRtool/CLUS_tool/')
-sys.path.insert(0,'/home/fedefab/Scrivania/Research/Post-doc/git/WRtool/CLUS_tool/WRtool/')
+cart_out_WRtool = '/home/fabiano/Research/lavori/WeatherRegimes/OUT_WRTOOL/'
+#cart_out_WRtool = '/home/fedefab/Scrivania/Research/Post-doc/Prima_WRTool/Visual_traj/'
+cart_git_WRtool = '/home/fabiano/Research/git/WRtool/'
+#cart_git_WRtool = '/home/fedefab/Scrivania/Research/Post-doc/git/WRtool/'
+
+sys.path.insert(0,cart_git_WRtool+'CLUS_tool/')
+sys.path.insert(0,cart_git_WRtool+'CLUS_tool/WRtool/')
 import lib_WRtool as lwr
 import clus_manipulate as clum
 import readsavencfield as rsnc
@@ -36,7 +41,7 @@ numclus = 4
 dim = 3
 timeout = 100
 
-cart1 = '/home/fedefab/Scrivania/Research/Post-doc/Prima_WRTool/Visual_traj/ERA_ref/'
+cart1 = cart_out_WRtool+'ERA_ref/'
 nameout = 'zg500_day_ERAInterim_144x73_1ens_DJF_EAT_1979-2008'
 
 ifile = cart1 + 'area_anomaly_'+nameout+'_{}.nc'.format(0)
@@ -52,7 +57,7 @@ pvec_anom_ERA = np.array(clum.compute_pvectors(dim, solver_ERA, anomalies_ERA))
 pvec_clus_ERA = np.array(clum.compute_pvectors(dim, solver_ERA, clus_ERA))
 
 
-cart2 = '/home/fedefab/Scrivania/Research/Post-doc/Prima_WRTool/Visual_traj/OUT_ECEARTH_EC1279/'
+cart2 = cart_out_WRtool+ 'OUT_ECEARTH_EC1279/'
 nameout = 'zg500_day_ECEARTH_144x73_1ens_DJF_EAT_1979-2008'
 
 ifile = cart2+'area_anomaly_'+nameout+'_{}.nc'.format(0)
@@ -68,27 +73,33 @@ pvec_anom_EC = np.array(clum.compute_pvectors(dim, solver_ERA, anomalies_EC))
 pvec_clus_EC = np.array(clum.compute_pvectors(dim, solver_ERA, clus_EC))
 
 
-cmap = cm.get_cmap('jet')
-colors = [cmap(0.0), cmap(0.2), cmap(0.8), cmap(1.0)]
+cmap = cm.get_cmap('nipy_spectral')
 
 plt.ion()
+# all_col = [cmap(co) for co in np.linspace(0,1,20)]
+# plt.figure()
+# for co, pu in zip(all_col, range(len(all_col))):
+#     plt.scatter(pu*1.0/20, 0., color = co, s = 100)
+
+colors = [cmap(0.05), cmap(0.25), cmap(0.45), cmap(0.8)]
+
 
 if dim == 2:
     fig = plt.figure(figsize=(8, 6), dpi=150)
     for clu in range(numclus):
         cluok = (ind_ERA == clu)
         color = colors[clu]
-        plt.scatter(pvec_anom_ERA[cluok, 0], pvec_anom_ERA[cluok, 1], color = color, s = 2)
-    plt.scatter(pvec_clus_ERA[:,0], pvec_clus_ERA[:,1], color = 'red', s = 20)
+        plt.scatter(pvec_anom_ERA[cluok, 0], pvec_anom_ERA[cluok, 1], color = color, s = 1)
+    plt.scatter(pvec_clus_ERA[:,0], pvec_clus_ERA[:,1], color = colors, s = 30)
 
     fig2 = plt.figure(figsize=(8, 6), dpi=150)
     for clu in range(numclus):
         cluok = (ind_EC == clu)
         color = colors[clu]
-        plt.scatter(pvec_anom_EC[cluok, 0], pvec_anom_EC[cluok, 1], color = color, s = 2)
+        plt.scatter(pvec_anom_EC[cluok, 0], pvec_anom_EC[cluok, 1], color = color, s = 1)
 
-    plt.scatter(pvec_clus_EC[:,0], pvec_clus_EC[:,1], color = 'blue', s = 20)
-    plt.scatter(pvec_clus_ERA[:,0], pvec_clus_ERA[:,1], color = 'red', s = 20)
+    plt.scatter(pvec_clus_EC[:,0], pvec_clus_EC[:,1], color = colors, s = 10)
+    plt.scatter(pvec_clus_ERA[:,0], pvec_clus_ERA[:,1], color = colors, s = 30)
 elif dim == 3:
     fig = plt.figure(figsize=(8, 6), dpi=150)
     ax = fig.add_subplot(111, projection='3d')
@@ -96,13 +107,13 @@ elif dim == 3:
     for clu in range(numclus):
         cluok = (ind_ERA == clu)
         color = colors[clu]
-        ax.scatter(pvec_anom_ERA[cluok, 0], pvec_anom_ERA[cluok, 1], pvec_anom_ERA[cluok, 2], s = 2, color = color)
+        ax.scatter(pvec_anom_ERA[cluok, 0], pvec_anom_ERA[cluok, 1], pvec_anom_ERA[cluok, 2], s = 1, color = color)
 
     ax.set_xlabel('EOF 1')
     ax.set_ylabel('EOF 2')
     ax.set_zlabel('EOF 3')
 
-    ax.scatter(pvec_clus_ERA[:,0], pvec_clus_ERA[:,1], pvec_clus_ERA[:,2], color = 'red', s = 20)
+    ax.scatter(pvec_clus_ERA[:,0], pvec_clus_ERA[:,1], pvec_clus_ERA[:,2], color = colors, s = 30)
 
     fig2 = plt.figure(figsize=(8, 6), dpi=150)
     ax = fig2.add_subplot(111, projection='3d')
@@ -110,17 +121,24 @@ elif dim == 3:
     for clu in range(numclus):
         cluok = (ind_EC == clu)
         color = colors[clu]
-        ax.scatter(pvec_anom_EC[cluok, 0], pvec_anom_EC[cluok, 1], pvec_anom_EC[cluok, 2], s = 2, color = color)
+        ax.scatter(pvec_anom_EC[cluok, 0], pvec_anom_EC[cluok, 1], pvec_anom_EC[cluok, 2], s = 1, color = color)
 
     ax.set_xlabel('EOF 1')
     ax.set_ylabel('EOF 2')
     ax.set_zlabel('EOF 3')
 
-    ax.scatter(pvec_clus_ERA[:,0], pvec_clus_ERA[:,1], pvec_clus_ERA[:,2], color = 'red', s = 20)
-    ax.scatter(pvec_clus_EC[:,0], pvec_clus_EC[:,1], pvec_clus_EC[:,2], color = 'blue', s = 20)
+    ax.scatter(pvec_clus_ERA[:,0], pvec_clus_ERA[:,1], pvec_clus_ERA[:,2], color = colors, s = 30)
+    ax.scatter(pvec_clus_EC[:,0], pvec_clus_EC[:,1], pvec_clus_EC[:,2], color = colors, s = 10)
 
-#data = pvec_anom_ERA
-data = pvec_anom_EC
+
+data = pvec_anom_ERA
+nome = 'ERA'
+cart_out = cart1
+save = False
+
+#data = pvec_anom_EC
+#nome = 'EC'
+#cart_out = cart2
 
 print(data.shape)
 fig = plt.figure(figsize=(8, 6), dpi=150)
@@ -157,31 +175,31 @@ def update_lines(num, data, line, testa, ind_col, colors, numclus, timout):
     return
 
 
-ax.scatter(pvec_clus_ERA[:,0], pvec_clus_ERA[:,1], pvec_clus_ERA[:,2], color = 'red', s = 20)
+ax.scatter(pvec_clus_ERA[:,0], pvec_clus_ERA[:,1], pvec_clus_ERA[:,2], color = colors, s = 30)
 
 line = ax.plot(data[0:5, 0], data[0:5, 1], data[0:5, 2], color = 'grey')[0]
 testa = ax.scatter(data[4, 0], data[4, 1], data[4, 2], color = colors[int(ind_ERA[4])], s = 30)
 
 ax.set_title('EAT geopotential 3D Trajectory')
 
-ax.set_xlim3d([-5000., 5000.])
+ax.set_xlim3d([-4000., 4000.])
 ax.set_xlabel('EOF 1')
 
-ax.set_ylim3d([-5000., 5000.])
+ax.set_ylim3d([-4000., 4000.])
 ax.set_ylabel('EOF 2')
 
-ax.set_zlim3d([-5000., 5000.])
+ax.set_zlim3d([-4000., 4000.])
 ax.set_zlabel('EOF 3')
 
-# Creating the Animation object
-# line_ani = animation.FuncAnimation(fig, update_lines, len(data[:,0]), fargs=(data, line, testa, ind_ERA, colors, numclus, timeout), interval=50, blit=False)
 
-#metadata = dict(title='ERA trajectory in 3D EOF space', artist='Federico Fabiano')
-metadata = dict(title='EC trajectory in 3D EOF space', artist='Federico Fabiano')
-writer = gif_maker(fps = 20, metadata = metadata)
-
-with writer.saving(fig, cart2 + "EC_trajectory.gif", 100):
-    for i in range(len(data[:,0])):
-        print(i)
-        update_lines(i, data, line, testa, ind_ERA, colors, numclus, timeout)
-        writer.grab_frame()
+if save:
+    metadata = dict(title='{} trajectory in 3D EOF space'.format(nome), artist='Federico Fabiano')
+    writer = gif_maker(fps = 20, metadata = metadata)
+    with writer.saving(fig, cart_out + "{}_trajectory.gif".format(nome), 100):
+        for i in range(len(data[:,0])):
+            print(i)
+            update_lines(i, data, line, testa, ind_ERA, colors, numclus, timeout)
+            writer.grab_frame()
+else:
+    # Creating the Animation object
+    line_ani = animation.FuncAnimation(fig, update_lines, len(data[:,0]), fargs=(data, line, testa, ind_ERA, colors, numclus, timeout), interval=50, blit=False)
