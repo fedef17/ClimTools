54d903fc74ffa21ee0201fa9e40b5255c9bc3ee5
Federico Fabiano <f.fabiano@isac.cnr.it>
2018-07-24 18:23:20 +0200

    Improved visual traj

--- test_trajectory_visualization.py
+++ test_trajectory_visualization.py
@@ -22,7 +22,7 @@ from copy import deepcopy as cp
 import matplotlib.cm as cm
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
-from matplotlib.animation import ImageMagickWriter as gif_maker
+from matplotlib.animation import ImageMagickFileWriter, FFMpegFileWriter, AVConvFileWriter
 
 cart_out_WRtool = '/home/fabiano/Research/lavori/WeatherRegimes/OUT_WRTOOL/'
 #cart_out_WRtool = '/home/fedefab/Scrivania/Research/Post-doc/Prima_WRTool/Visual_traj/'
@@ -40,6 +40,8 @@ numpcs = 4
 numclus = 4
 dim = 3
 timeout = 100
+alpha = 0.3
+alpha2 = 0.6
 
 cart1 = cart_out_WRtool+'ERA_ref/'
 nameout = 'zg500_day_ERAInterim_144x73_1ens_DJF_EAT_1979-2008'
@@ -47,6 +49,7 @@ nameout = 'zg500_day_ERAInterim_144x73_1ens_DJF_EAT_1979-2008'
 ifile = cart1 + 'area_anomaly_'+nameout+'_{}.nc'.format(0)
 _ = rsnc.read3Dncfield(ifile)
 anomalies_ERA = _[0]
+dates = _[3]
 
 solver_ERA = lwr.read_out_compute(cart1, nameout, numpcs)
 
@@ -81,7 +84,7 @@ plt.ion()
 # for co, pu in zip(all_col, range(len(all_col))):
 #     plt.scatter(pu*1.0/20, 0., color = co, s = 100)
 
-colors = [cmap(0.05), cmap(0.25), cmap(0.45), cmap(0.8)]
+colors = [cmap(0.25), cmap(0.85), cmap(0.05), cmap(0.45)]
 
 
 if dim == 2:
@@ -107,13 +110,15 @@ elif dim == 3:
     for clu in range(numclus):
         cluok = (ind_ERA == clu)
         color = colors[clu]
-        ax.scatter(pvec_anom_ERA[cluok, 0], pvec_anom_ERA[cluok, 1], pvec_anom_ERA[cluok, 2], s = 1, color = color)
+        ax.scatter(pvec_anom_ERA[cluok, 0], pvec_anom_ERA[cluok, 1], pvec_anom_ERA[cluok, 2], s = 1, color = color, alpha = alpha)
 
     ax.set_xlabel('EOF 1')
     ax.set_ylabel('EOF 2')
     ax.set_zlabel('EOF 3')
 
-    ax.scatter(pvec_clus_ERA[:,0], pvec_clus_ERA[:,1], pvec_clus_ERA[:,2], color = colors, s = 30)
+    ax.scatter(pvec_clus_ERA[:,0], pvec_clus_ERA[:,1], pvec_clus_ERA[:,2], color = colors, s = 40)
+
+    fig.savefig(cart1+'ERA_cloudplot_clusters.pdf', format = 'pdf')
 
     fig2 = plt.figure(figsize=(8, 6), dpi=150)
     ax = fig2.add_subplot(111, projection='3d')
@@ -121,20 +126,22 @@ elif dim == 3:
     for clu in range(numclus):
         cluok = (ind_EC == clu)
         color = colors[clu]
-        ax.scatter(pvec_anom_EC[cluok, 0], pvec_anom_EC[cluok, 1], pvec_anom_EC[cluok, 2], s = 1, color = color)
+        ax.scatter(pvec_anom_EC[cluok, 0], pvec_anom_EC[cluok, 1], pvec_anom_EC[cluok, 2], s = 1, color = color, alpha = alpha)
 
     ax.set_xlabel('EOF 1')
     ax.set_ylabel('EOF 2')
     ax.set_zlabel('EOF 3')
 
-    ax.scatter(pvec_clus_ERA[:,0], pvec_clus_ERA[:,1], pvec_clus_ERA[:,2], color = colors, s = 30)
-    ax.scatter(pvec_clus_EC[:,0], pvec_clus_EC[:,1], pvec_clus_EC[:,2], color = colors, s = 10)
+    ax.scatter(pvec_clus_ERA[:,0], pvec_clus_ERA[:,1], pvec_clus_ERA[:,2], color = colors, s = 40)
+    ax.scatter(pvec_clus_EC[:,0], pvec_clus_EC[:,1], pvec_clus_EC[:,2], color = colors, s = 20)
+
+    fig2.savefig(cart2+'EC_cloudplot_clusters.pdf', format = 'pdf')
 
 
 data = pvec_anom_ERA
 nome = 'ERA'
 cart_out = cart1
-save = False
+save = True
 
 #data = pvec_anom_EC
 #nome = 'EC'
@@ -144,7 +151,7 @@ print(data.shape)
 fig = plt.figure(figsize=(8, 6), dpi=150)
 ax = fig.add_subplot(111, projection='3d')
 
-def update_lines(num, data, line, testa, ind_col, colors, numclus, timout):
+def update_lines(num, data, line, testa, ind_col, colors, numclus, timeout, showdate, dates):
     if num > timeout:
         init = num-timeout
     elif num < 5:
@@ -170,6 +177,9 @@ def update_lines(num, data, line, testa, ind_col, colors, numclus, timout):
     testa._facecolor3d = color
     testa._edgecolor3d = color
 
+    nudate = dates[num-1].ctime()[4:11]+dates[num-1].ctime()[-5:]
+    showdate.set_text(nudate)
+
     # testa = ax.scatter(data[num, 0], data[num, 1], data[num, 2], color = colors[int(ind_ERA[num])], s = 20)
 
     return
@@ -177,7 +187,10 @@ def update_lines(num, data, line, testa, ind_col, colors, numclus, timout):
 
 ax.scatter(pvec_clus_ERA[:,0], pvec_clus_ERA[:,1], pvec_clus_ERA[:,2], color = colors, s = 30)
 
-line = ax.plot(data[0:5, 0], data[0:5, 1], data[0:5, 2], color = 'grey')[0]
+nudate = dates[4].ctime()[4:11]+dates[4].ctime()[-5:]
+showdate = ax.text(-4000., 4000., 3000., nudate, bbox=dict(facecolor='lightsteelblue', edgecolor='black', boxstyle='round,pad=1'))
+
+line = ax.plot(data[0:5, 0], data[0:5, 1], data[0:5, 2], color = 'grey', alpha = alpha2)[0]
 testa = ax.scatter(data[4, 0], data[4, 1], data[4, 2], color = colors[int(ind_ERA[4])], s = 30)
 
 ax.set_title('EAT geopotential 3D Trajectory')
@@ -194,12 +207,14 @@ ax.set_zlabel('EOF 3')
 
 if save:
     metadata = dict(title='{} trajectory in 3D EOF space'.format(nome), artist='Federico Fabiano')
-    writer = gif_maker(fps = 20, metadata = metadata)
+    #writer = AVConvFileWriter(fps = 20, metadata = metadata)
+    #writer = FFMpegFileWriter(fps = 20, metadata = metadata)
+    writer = ImageMagickFileWriter(fps = 20, metadata = metadata)
     with writer.saving(fig, cart_out + "{}_trajectory.gif".format(nome), 100):
         for i in range(len(data[:,0])):
             print(i)
-            update_lines(i, data, line, testa, ind_ERA, colors, numclus, timeout)
+            update_lines(i, data, line, testa, ind_ERA, colors, numclus, timeout, showdate, dates)
             writer.grab_frame()
 else:
     # Creating the Animation object
-    line_ani = animation.FuncAnimation(fig, update_lines, len(data[:,0]), fargs=(data, line, testa, ind_ERA, colors, numclus, timeout), interval=50, blit=False)
+    line_ani = animation.FuncAnimation(fig, update_lines, len(data[:,0]), fargs=(data, line, testa, ind_ERA, colors, numclus, timeout, showdate, dates), interval=50, blit=False)
