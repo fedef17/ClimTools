11db4c8cab56dea8150bdb33454a348539767d6a
Federico Fabiano <f.fabiano@isac.cnr.it>
2020-09-28 12:33:44 +0200

    bug with is_ensemble

--- climdiags.py
+++ climdiags.py
@@ -333,22 +333,20 @@ def export_results_to_json(filename, results):
         try:
             json.dump(nures, fp)
         except Exception as expc:
-            mod = list(results.keys())[0]
-            if mod == 'reference':
-                mod = list(results.keys())[1]
             print(results.keys())
-            print(mod, results[mod])
-            ens = nures[mod]['ens_names'][0]
-            print(nures[mod]['ens_names'])
-            for ke in nures[mod].keys():
-                if type(nures[mod][ke]) is dict:
-                    tip = type(nures[mod][ke][ens])
-                    if tip == list:
-                        print(ke, tip, type(nures[mod][ke][ens][0]))
+            for mod in results.keys():
+                print(mod, results[mod])
+                ens = nures[mod]['ens_names'][0]
+                print(nures[mod]['ens_names'])
+                for ke in nures[mod].keys():
+                    if type(nures[mod][ke]) is dict:
+                        tip = type(nures[mod][ke][ens])
+                        if tip == list:
+                            print(ke, tip, type(nures[mod][ke][ens][0]))
+                        else:
+                            print(ke, tip)
                     else:
-                        print(ke, tip)
-                else:
-                    print(ke, type(nures[mod][ke]))
+                        print(ke, type(nures[mod][ke]))
 
             raise expc
 
