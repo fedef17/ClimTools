d0d959f9445c5f15aefa44578c26af1fea637948
Federico Fabiano <f.fabiano@isac.cnr.it>
2023-06-20 15:33:49 +0200

    fixed map in notebook

--- climtools_lib.py
+++ climtools_lib.py
@@ -6749,7 +6749,7 @@ def plot_map_contour(data, lat = None, lon = None, filename = None, visualizatio
         fig4.savefig(filename)
         # plt.close(fig4)
 
-    if 'vscod' in os.environ['path']:
+    if 'vscod' in os.environ['PATH']:
         # matplotlib inline in jupyter
         return ax
 
